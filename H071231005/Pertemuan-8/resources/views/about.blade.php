b127221b1de93ebb063331853b77aa60f67f7198
NurulWahdania <nurulwahdania182@gmail.com>
2024-11-01 15:31:20 +0800

    Praktikum 8

---
+++ H071231005/Pertemuan-8/resources/views/about.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts/main')
+@section('content')
+<section id="about"></section>
+<div class="p-16">
+    <div class="font-mono md:p-16">
+        <h2 class="font-bold">ABOUT</h2>
+        <div class="md:flex ">
+            <div class="text-xs text-justify mt-3 md:text-sm md:mt-0 md:p-8">
+                <p>Minecraft is a sandbox game that allows players to build anything they want using blocks. The game has a creative mode, where players have unlimited access to all resources, and a survival mode, where players must gather resources and build shelters to survive the monsters that appear at night. Minecraft also has many other features, such as the ability to craft, grow crops and raise animals.</p>
+            </div>
+            <div class="text-xs text-justify mt-8 md:text-sm md:mt-0 md:p-8">
+                <p>Minecraft is a popular game worldwide, with millions of players around the world. The game has been praised for its creative and open nature, which allows players to express themselves and create their own worlds. Minecraft has also been used as an educational tool, as it can help children learn about design, architecture and coding.</p>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+@endsection
