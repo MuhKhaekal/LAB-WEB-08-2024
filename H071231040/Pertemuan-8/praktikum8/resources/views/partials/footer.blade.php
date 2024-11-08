42fa7f5e5d3b53d287c821997900c53e8d523b86
Cholyn Sharon Enos <sharoncholyn@gmail.com>
2024-11-08 20:37:18 +0800

    Task 8 : Laravel MVC and Database Connection

---
+++ H071231040/Pertemuan-8/praktikum8/resources/views/partials/footer.blade.php
@@ -0,0 +1,56 @@
+<footer>
+    <div class="px-4 py-6 bg-gray-100 dark:bg-black md:flex md:items-center md:justify-between">
+        <span class="text-sm text-black dark:text-gray-300 sm:text-center">&copy; 2013 <a
+                href="https://www.rockstargames.com/">Rockstar Games™</a>. All Rights Reserved.
+        </span>
+        <div class="flex mt-4 sm:justify-center md:mt-0 space-x-5 rtl:space-x-reverse">
+            <a href="" class="dark:text-gray-400 hover:text-gray-900 dark:hover:text-white" target="_blank">
+                <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor"
+                    viewBox="0 0 8 19">
+                    <path fill-rule="evenodd"
+                        d="M6.135 3H8V0H6.135a4.147 4.147 0 0 0-4.142 4.142V6H0v3h2v9.938h3V9h2.021l.592-3H5V3.591A.6.6 0 0 1 5.592 3h.543Z"
+                        clip-rule="evenodd" />
+                </svg>
+                <span class="sr-only">Facebook page</span>
+            </a>
+            <a href="https://www.youtube.com/@RockstarGames" class="dark:text-gray-400 hover:text-gray-900 dark:hover:text-white" target="_blank">
+                <svg class="w-[20px] h-[20px]" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" 
+                    viewBox="2 2 24 24">
+                    <path fill-rule="evenodd"
+                        d="M21.7 8.037a4.26 4.26 0 0 0-.789-1.964 2.84 2.84 0 0 0-1.984-.839c-2.767-.2-6.926-.2-6.926-.2s-4.157 0-6.928.2a2.836 2.836 0 0 0-1.983.839 4.225 4.225 0 0 0-.79 1.965 30.146 30.146 0 0 0-.2 3.206v1.5a30.12 30.12 0 0 0 .2 3.206c.094.712.364 1.39.784 1.972.604.536 1.38.837 2.187.848 1.583.151 6.731.2 6.731.2s4.161 0 6.928-.2a2.844 2.844 0 0 0 1.985-.84 4.27 4.27 0 0 0 .787-1.965 30.12 30.12 0 0 0 .2-3.206v-1.516a30.672 30.672 0 0 0-.202-3.206Zm-11.692 6.554v-5.62l5.4 2.819-5.4 2.801Z"
+                        clip-rule="evenodd" />
+                </svg>
+                <span class="sr-only">YouTube Channel</span>
+            </a>
+            <a href="https://x.com/RockstarGames" class="dark:text-gray-400 hover:text-gray-900 dark:hover:text-white" target="_blank">
+                <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor"
+                    viewBox="0 0 24 24">
+                    <path fill-rule="evenodd"
+                        d="M13.795 10.533 20.68 2h-3.073l-5.255 6.517L7.69 2H1l7.806 10.91L1.47 22h3.074l5.705-7.07L15.31 22H22l-8.205-11.467Zm-2.38 2.95L9.97 11.464 4.36 3.627h2.31l4.528 6.317 1.443 2.02 6.018 8.409h-2.31l-4.934-6.89Z" />
+                    clip-rule="evenodd" />
+                </svg>
+                <span class="sr-only">Twitter page</span>
+            </a>
+            <a href="https://www.instagram.com/rockstargames/"
+                class="dark:text-gray-400 hover:text-gray-900 dark:hover:text-white" target="_blank">
+                <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor"
+                    viewBox="0 2 25 25">
+                    <path fill="currentColor" fill-rule="evenodd"
+                        d="M3 8a5 5 0 0 1 5-5h8a5 5 0 0 1 5 5v8a5 5 0 0 1-5 5H8a5 5 0 0 1-5-5V8Zm5-3a3 3 0 0 0-3 3v8a3 3 0 0 0 3 3h8a3 3 0 0 0 3-3V8a3 3 0 0 0-3-3H8Zm7.597 2.214a1 1 0 0 1 1-1h.01a1 1 0 1 1 0 2h-.01a1 1 0 0 1-1-1ZM12 9a3 3 0 1 0 0 6 3 3 0 0 0 0-6Zm-5 3a5 5 0 1 1 10 0 5 5 0 0 1-10 0Z"
+                        clip-rule="evenodd" />
+                </svg>
+                <span class="sr-only">Instagram account</span>
+            </a>
+            <a href="https://www.rockstargames.com/gta-online?_gl=1*tc8r41*_gcl_au*MjEyMjk3MTg3Mi4xNzI2ODAzODcx*_ga*ODQ4MTM3ODU0LjE3MDA2MjY4Mzk.*_ga_PJQ2JYZDQC*MTczMDQ1NTQ4Ni4yMC4xLjE3MzA0NTU2MTUuMC4wLjA."
+                class="dark:text-gray-400 hover:text-gray-900 dark:hover:text-white" target="_blank">
+                <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor"
+                    viewBox="0 0 20 20">
+                    <path fill-rule="evenodd"
+                        d="M10 0a10 10 0 1 0 10 10A10.009 10.009 0 0 0 10 0Zm6.613 4.614a8.523 8.523 0 0 1 1.93 5.32 20.094 20.094 0 0 0-5.949-.274c-.059-.149-.122-.292-.184-.441a23.879 23.879 0 0 0-.566-1.239 11.41 11.41 0 0 0 4.769-3.366ZM8 1.707a8.821 8.821 0 0 1 2-.238 8.5 8.5 0 0 1 5.664 2.152 9.608 9.608 0 0 1-4.476 3.087A45.758 45.758 0 0 0 8 1.707ZM1.642 8.262a8.57 8.57 0 0 1 4.73-5.981A53.998 53.998 0 0 1 9.54 7.222a32.078 32.078 0 0 1-7.9 1.04h.002Zm2.01 7.46a8.51 8.51 0 0 1-2.2-5.707v-.262a31.64 31.64 0 0 0 8.777-1.219c.243.477.477.964.692 1.449-.114.032-.227.067-.336.1a13.569 13.569 0 0 0-6.942 5.636l.009.003ZM10 18.556a8.508 8.508 0 0 1-5.243-1.8 11.717 11.717 0 0 1 6.7-5.332.509.509 0 0 1 .055-.02 35.65 35.65 0 0 1 1.819 6.476 8.476 8.476 0 0 1-3.331.676Zm4.772-1.462A37.232 37.232 0 0 0 13.113 11a12.513 12.513 0 0 1 5.321.364 8.56 8.56 0 0 1-3.66 5.73h-.002Z"
+                        clip-rule="evenodd" />
+                </svg>
+                <span class="sr-only">Official website</span>
+            </a>
+        </div>
+    </div>
+</footer>
